37b254a41e3057894300552e0d70d87f4d085662
ElProfe <educaysoft@gmail.com>
2022-09-08 14:18:50 -0500

    base de datos

---
+++ datos.sql
@@ -0,0 +1,5 @@
+create database miplataforma;
+use miplataforma;
+
+create table persona(idpersona int,nombres varchar(20),apellidos varchar(20),cedula varchar(10),idgenero int,fechanacimiento date,idnacionalidad int,idetnia int,idestadocivil int);
+
