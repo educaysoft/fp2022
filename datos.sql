eedcd986dbc863c6f316e8e72fd1660d18ae47bd
Stain Francis <educaysoft@gmail.com>
2022-09-12 11:09:35 -0400

    crea otra vez el suma.cpp y lo compile

--- datos.sql
+++
@@ -1,5 +0,0 @@
-create database miplataforma;
-use miplataforma;
-
-create table persona(idpersona int,nombres varchar(20),apellidos varchar(20),cedula varchar(10),idgenero int,fechanacimiento date,idnacionalidad int,idetnia int,idestadocivil int);
-
--- suma.cpp
+++ suma.cpp
@@ -1,1 +1,14 @@
-hola munos soy stalin
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float A,B,C;
+ cin>>A>>B;
+ C=A+B;
+ cout<<C;
+
+ return 0;
+
+}
