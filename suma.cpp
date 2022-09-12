e4fafc769eadc68fecc204baab178f852a7d85a2
Stain Francis <educaysoft@gmail.com>
2022-09-12 11:36:16 -0400

    mejoras en sumas.cpp

--- suma.cpp
+++ suma.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
 
- float A,B,C;
- cout<<"Ingrese 2 numero A B :";
- cin>>A>>B;
- C=A+B;
+ float x,y,z;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>x>>y;
+ z=x+y;
  cout<<"El resutado es : ";
- cout<<C;
+ cout<<z<<endl;
 
  return 0;
 
