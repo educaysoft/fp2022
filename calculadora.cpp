1bce9f09b0f22a444c834a7795b3c737b08e8101
Stain Francis <educaysoft@gmail.com>
2022-09-14 11:17:19 -0400

    modificacione al programa de multiplicacion

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,19 +1,29 @@
+//Programa que presenta las operaciones básicas
+//Creado por Stalin Francis 
+//Fecha: 14-09-2022
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	float x,y, s,r,m,d;
-	cout<<"Ingrese el valor de X="; cin>>x;
-	cout<<"Ingrese el valor de Y="; cin>>y;
+	float x,y,s,m,d,r;
+	//Ingreso de datos
+	cout<<"Ingrese en valor de x=:";
+	cin>>x;
+	cout<<"Ingrese en valor de y=:";
+	//Operaciones 
+	cin>>y;
 	s=x+y;
-	r=x-y;
 	m=x*y;
 	d=x/y;
-	cout<<"La suma de "<<x<<"+"<<y<<"="<<s<<endl;
-	cout<<"La resta de "<<x<<"-"<<y<<"="<<r<<endl;
-	cout<<"La multiplicacion de "<<x<<"*"<<y<<"="<<m<<endl;
-	cout<<"La division de "<<x<<"/"<<y<<"="<<d<<endl;
-	return 0;
+	r=x-y;
+	//Muestra los resutaldos.
+	cout<<"Las suma de "<<x<<" + "<<y<<" = "<<s<<endl;
+	cout<<"Las multiplicacion de "<<x<<" * "<<y<<" = "<<m<<endl;
+	cout<<"Las division de "<<x<<" / "<<y<<" = "<<d<<endl;
+	cout<<"Las resta de "<<x<<" - "<<y<<" = "<<r<<endl;
+	return 0 ;
 
 }
+
